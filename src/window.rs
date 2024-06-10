09820f82a38a3609dc2ba934f3f659b61d49962a
Steyn Mulder <steynmulder@gmail.com>
2024-06-10 12:24:38 +0200

    Implemented basic window

--- src/window.rs
+++ src/window.rs
@@ -1,5 +1,34 @@
+extern crate sdl2;
 
+use sdl2::pixels::Color;
+use sdl2::event::Event;
+use std::time::Duration;
 
 pub fn create_window() {
-    println!("Hello, world!");
+    // barebones from https://docs.rs/sdl2/latest/sdl2/
+    let sdl_context = sdl2::init().unwrap();
+    let video_subsystem = sdl_context.video().unwrap();
+
+    let window = video_subsystem.window("visualizer", 800, 600)
+        .position_centered()
+        .build()
+        .unwrap();
+
+    let mut canvas = window.into_canvas().build().unwrap();
+
+    canvas.set_draw_color(Color::RGB(255, 0, 128));
+    canvas.clear();
+    canvas.present();
+
+    let mut event_pump = sdl_context.event_pump().unwrap();
+
+    'running: loop {
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit {..} => {break 'running;},
+                _ => {}
+            }
+        }
+        ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
+    }
 }
