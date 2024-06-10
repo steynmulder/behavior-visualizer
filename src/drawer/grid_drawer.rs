cf4b7eee02fb0b78d172c1ee6d37e2fb4c5e3b42
Steyn Mulder <steynmulder@gmail.com>
2024-06-10 15:50:20 +0200

    Implemented grid drawing

--- src/drawer/grid_drawer.rs
+++ src/drawer/grid_drawer.rs
@@ -2,8 +2,17 @@ extern crate sdl2;
 use sdl2::render::Canvas;
 use sdl2::video::Window;
 use sdl2::pixels::Color;
+use sdl2::rect::Rect;
 
-pub fn draw_grid(canvas: &mut Canvas<Window>) {
-    canvas.set_draw_color(Color::RGB(100, 100, 0));
-    canvas.clear();
+pub fn draw_grid(canvas: &mut Canvas<Window>, width: &u32, height: &u32) {
+    // canvas.set_draw_color(Color::RGB(255, 255, 255));
+    // canvas.clear();
+    let cell_size = 16;
+    canvas.set_draw_color(Color::RGB(0, 0, 0));
+    for i in 0..*width/cell_size {
+        let _ = canvas.fill_rect(Rect::new((i * cell_size) as i32, 0, 3, *height));
+    }
+    for j in 0..*height/cell_size {
+        let _ = canvas.fill_rect(Rect::new(0, (j * cell_size) as i32, *width, 3));
+    }
 }
--- src/window/window.rs
+++ src/window/window.rs
@@ -5,21 +5,23 @@ use sdl2::event::Event;
 use std::time::Duration;
 use crate::drawer::grid_drawer;
 
-
-
 pub fn create_window() {
+    const WIDTH: u32 = 1920;
+    const HEIGHT: u32 = 960;
+
     // barebones from https://docs.rs/sdl2/latest/sdl2/
     let sdl_context = sdl2::init().unwrap();
     let video_subsystem = sdl_context.video().unwrap();
 
-    let window = video_subsystem.window("visualizer", 800, 600)
-        .position_centered()
+    let window = video_subsystem.window("visualizer", WIDTH, HEIGHT)
+        // .position_centered()
+        .position(0, 40)
         .build()
         .unwrap();
 
     let mut canvas = window.into_canvas().build().unwrap();
 
-    canvas.set_draw_color(Color::RGB(255, 0, 128));
+    canvas.set_draw_color(Color::RGB(255, 255, 255));
     canvas.clear();
     canvas.present();
 
@@ -32,7 +34,7 @@ pub fn create_window() {
                 _ => {}
             }
         }
-        grid_drawer::draw_grid(&mut canvas);
+        grid_drawer::draw_grid(&mut canvas, &WIDTH, &HEIGHT);
         canvas.present();
         ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
     }
